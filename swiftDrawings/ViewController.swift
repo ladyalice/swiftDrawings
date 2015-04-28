be303fbb585e5f161c5842eb7955f62e07a5b4c1
ladyalice <alicejoyk@gmail.com>
2015-04-28 16:58:04 -0700

    Initial Commit

---
+++ swiftDrawings/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftDrawings
+//
+//  Created by Alice Karsevar on 4/28/15.
+//  Copyright (c) 2015 Best Day Ever. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
