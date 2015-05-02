36b18b7e27d199e848fa5dca3e089fa127412257
ladyalice <alicejoyk@gmail.com>
2015-05-01 17:59:32 -0700

    Initial Commit
    Carl saying things.

--- swiftDrawings/drawings.swift
+++ swiftDrawings/drawings.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  drawings.swift
 //  swiftDrawings
 //
 //  Created by Alice Karsevar on 4/28/15.
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    func drawRect(rect: CGRect) {
+class drawings: UIView {
+
+    
+    override func drawRect(rect: CGRect) {
         //context is the object used for drawing
         let context = UIGraphicsGetCurrentContext()
         CGContextSetLineWidth(context, 3.0)
@@ -17,28 +19,40 @@ class ViewController: UIViewController {
         
         //create a path
         
-       // CGContextMoveToPoint(context, 100, 60)
-       // CGContextAddLineToPoint(context, 400, 320)
+       CGContextMoveToPoint(context, 100, 60)
+       CGContextAddLineToPoint(context, 400, 320)
         
         
         //create a rectngle
-        let rec = CGRectMake(50, 50, 200, 400)
-        CGContextAddRect(context, rec)
+        //  let rec = CGRectMake(50, 50, 200, 400)
+        // CGContextAddRect(context, rec)
         
         //actually draw the path
         CGContextStrokePath(context)
         
+        
+        
+        //background image
+        let space = UIImage(named: "space1.jpg")
+        //get reference of screen dynamically so you know for images etc
+        let screen = UIScreen.mainScreen().bounds
+        space?.drawInRect(screen)
+        
+        
+        
         //need to turn it into a UIImage
         let smartMan = UIImage(named: "carl.jpg")
+        
         //figure out top point for image
-        let location = CGPointMake(25, 25)
+        let location = CGPointMake(75, 100)
         smartMan?.drawAtPoint(location)
         
         
-
+        
+    
+        
+        
+        
     }
-    }
 
-
-
-
+}
